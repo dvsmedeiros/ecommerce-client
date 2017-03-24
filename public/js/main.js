eedc40db3624aee1082911cb434950535a1c76ec
Davisson <dvisson.medeiros@gmail.com>
2017-03-23 22:35:02 -0300

    Add dependências da lib de mascaras

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-angular.module('ecommerce', ['ngRoute', 'ecommerceServices'])
+angular.module('ecommerce', ['ngRoute', 'ecommerceServices', 'ui.utils.masks'])
 	.config(function($routeProvider, $locationProvider) {
 
 		//$locationProvider.html5Mode(true);
