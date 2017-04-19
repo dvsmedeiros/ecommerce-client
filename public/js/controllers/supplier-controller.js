bd3e2756a3a0ffbf06dca6693fa4d5cfcf8d3212
Davisson <dvisson.medeiros@gmail.com>
2017-04-18 22:47:23 -0300

    Implementa View para cadastro de fornecedor

--- public/js/controllers/supplier-controller.js
+++ public/js/controllers/supplier-controller.js
@@ -1,5 +1,44 @@
-angular.module('ecommerce').controller('SupplierController', function($scope){
+angular.module('ecommerce').controller('SupplierController', function($scope, viaCepResource){
 	
 	$scope.message = '';
+	$scope.supplier = {
+		address : {}
+	};
+
+	$scope.submit = function(){
+		alert("BLA");
+	}
+
+	$scope.findAddressByZipcode = function (cep) {
+      
+      viaCepResource.get({cep: cep}, function(endereco) {
+        
+        if(endereco.erro === true){
+
+          $scope.supplier.address = {}
+
+        } else {
+
+          $scope.supplier.address = {
+
+            street : endereco.logradouro,
+            zipCode : endereco.cep,
+            neighborhood : {
+              name : endereco.bairro,
+              city : {	
+                description : endereco.localidade,
+                state : {
+                  code : endereco.uf
+                }
+              }
+            }
+          }
+
+        }
+      }, function(erro) {
+        $scope.supplier.address = {}
+        console.log(erro);
+      });
+    }
 
 });
