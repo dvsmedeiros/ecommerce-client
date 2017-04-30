1681b3e2fe1e2614d39621019c2a8c03d9c1eb45
Davisson <dvisson.medeiros@gmail.com>
2017-04-29 21:42:31 -0300

    Add carregamento de produtos por categoria

--- public/js/controllers/princial-controller.js
+++
@@ -1,3 +0,0 @@
-angular.module('ecommerce').controller('PrincipalController', function($scope){
-	
-});
--- public/js/controllers/principal-controller.js
+++ public/js/controllers/principal-controller.js
@@ -1,3 +1,26 @@
-angular.module('ecommerce').controller('PrincipalController', function($scope){
+angular.module('ecommerce').controller('PrincipalController', function($scope, productResource, categoryResource){
+
+	$scope.activeProducts = [];
+	$scope.activeCategories = [];
+
+	categoryResource.query({active: true}, function(categories){
+		$scope.activeCategories = categories;
+	}, function(error){
+		console.log(error);
+	});
+
+	productResource.query({active: true},function(products){
+		$scope.activeProducts = products;
+	}, function(error){
+		console.log(error);
+	});
+
+	$scope.findProductsByCategory = function(categoryId){
+		productResource.query({active: true, categoryId: categoryId}, function(products){
+			$scope.activeProducts = products;
+		}, function(error){
+			console.log(error);
+		});
+	}
 	
 });
