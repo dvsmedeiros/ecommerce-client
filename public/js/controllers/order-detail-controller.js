e2437c08831457584f8c429478c5b79c98f6b250
Davisson <dvisson.medeiros@gmail.com>
2017-05-05 23:30:00 -0300

    Add controller de detalhe do pedido

--- public/js/controllers/order-detail-controller.js
+++ public/js/controllers/order-detail-controller.js
@@ -1,29 +1,13 @@
-angular.module('ecommerce').controller('OrderDetailController', function($scope){
+angular.module('ecommerce').controller('OrderDetailController', function($scope, $routeParams, orderResource){
 
-	$scope.order = {
+    $scope.order = {};
 
-		id : 12456789,
-		quantity : 5,
-		date : '25/10/2017',
-		total : 176.50,
-		status : 'Entregue',
-		payment : 'Cartão de Crédito'
-	}
-
-	$scope.adress = {
-		
-        firstName : 'Davisson',
-        lastName : 'Medeiros',
-        street : 'Avenida Presbítero Eliezer Martins Costa',
-        number : '179',
-        zipcode : '08696-100',
-        neighborhood : 'Jardim Varan',
-        city : 'Suzano',
-        state : 'SP',
-        phoneNumber : {
-          ddd :  '11',
-          number : '4749-7851' 
-        }
+    if($routeParams.orderId) {
+        orderResource.get({orderId: $routeParams.orderId}, function(order) {
+        $scope.order = order; 
+        }, function(erro) {
+            console.log(erro);
+        });
     }
 
 });
