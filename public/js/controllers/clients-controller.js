f8271b8e83a461a36bc2793766f8a1ab0dfea09b
Davisson <dvisson.medeiros@gmail.com>
2017-10-24 21:32:06 -0200

    add funcionalidade de usuários/clientes

---
+++ public/js/controllers/clients-controller.js
@@ -0,0 +1,91 @@
+angular.module('ecommerce').controller('ClientsController', function($scope, $location, $routeParams, clientResource){
+	
+	$scope.client = {};
+	$scope.types = [];
+	$scope.filter = {
+		entity : {}
+	}
+	$scope.responseMessage = {
+		message : '',
+		hasError : true		
+	};
+
+	clientResource.logged(function(user) {
+      $scope.user = user; 
+    }, function(erro) {
+      $scope.responseMessage = erro.data;
+      console.log(erro);
+    });
+
+	clientResource.query(function(clients){
+		$scope.clients = clients;
+	}, function(error){
+		console.log(error);
+	});
+	
+	$scope.delete = function(client) {
+		
+		clientResource.delete({id: client.id}, function() {
+			var indexCategory = $scope.clients.indexOf(client);
+			$scope.clients.splice(indexCategory, 1);
+		}, function(erro) {
+			console.log(erro);
+		});
+		
+	};
+
+	$scope.inactivate = function(client) {
+		
+		var req = {
+			description : 'Inativado manualmente',
+			category : {
+				code : 'CAT0007'
+			}
+		}
+
+		if(client.id){
+
+			clientResource.inactivate({id: client.id}, req, function(status) {
+				client.active = false;
+			}, function(erro) {
+				console.log(erro);
+			});
+
+		}		
+	};
+
+	$scope.activate = function(client) {
+	
+		var req = {
+			description : 'Ativado manualmente',
+			category : {
+				code : 'CAT0007'
+			}
+		}
+
+		if(client.id){
+
+			client.active = true;
+			clientResource.update(client, function(status) {
+				client.active = true;
+			}, function(error) {
+				$scope.responseMessage = error.data;
+				console.log(error);
+			});
+		}	
+	};
+
+	$scope.findByFilter = function() {
+		var req = {
+			entity : $scope.filter.entity
+		}
+		clientResource.filter(req, function(clients) {
+			$scope.filter.entity = {};					
+			$scope.clients = clients
+		}, function(error) {					
+			$scope.responseMessage = error.data;
+			console.log(error);
+		});
+	};
+	
+});
