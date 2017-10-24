c46a982e120206159f02aba8c84cb4a3738a630d
Davisson <dvisson.medeiros@gmail.com>
2017-10-24 21:33:44 -0200

    corrige chamadas para web service

--- public/js/controllers/principal-controller.js
+++ public/js/controllers/principal-controller.js
@@ -1,22 +1,50 @@
-angular.module('ecommerce').controller('PrincipalController', function($scope, productResource, categoryResource){
+angular.module('ecommerce').controller('PrincipalController', function($scope, bookResource, categoryResource, clientResource){
 
 	$scope.activeProducts = [];
 	$scope.activeCategories = [];
 
-	categoryResource.query({active: true}, function(categories){
+	var filterCategory = {
+		entity : {
+			type : {
+				code : 'PROD'
+			},
+			active : true
+		}
+	}
+
+	clientResource.logged(function(user) {
+		$scope.user = user; 
+	}, function(erro) {
+		$scope.responseMessage = erro.data;
+		console.log(erro);
+	});	
+
+	categoryResource.filter(filterCategory, function(categories){
 		$scope.activeCategories = categories;
 	}, function(error){
 		console.log(error);
 	});
 
-	productResource.query({active: true},function(products){
+	var filterProduct = {
+		entity : {			
+			active : true				
+		}
+	}
+	bookResource.filter(filterProduct,function(products){
 		$scope.activeProducts = products;
 	}, function(error){
 		console.log(error);
 	});
 
-	$scope.findProductsByCategory = function(categoryId){
-		productResource.query({active: true, categoryId: categoryId}, function(products){
+	$scope.findProductsByCategory = function(id){
+
+		var filter = {
+			entity : {
+				id : id,
+				active : true				
+			}
+		}
+		bookResource.query(filter, function(products){
 			$scope.activeProducts = products;
 		}, function(error){
 			console.log(error);
