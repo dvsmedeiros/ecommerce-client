46a5dd7c07fcf9cc364a49e69781d63edeaa0a95
Davisson <dvisson.medeiros@gmail.com>
2017-11-10 23:53:00 -0200

    add filter barCode e isbn

--- public/js/filter.js
+++ public/js/filter.js
@@ -11,4 +11,16 @@ angular.module('ecommerce')
 			return input.replace(/^(\d{4})(\d{4})?(\d{4})?(\d{4})?/, "$1 $2 $3 $4");
 		};
 	})
+	.filter('isbn', function(){
+		return function(input){
+			var input = input || '';
+			return input.replace(/^(\d{3})(\d{2})?(\d{3})?(\d{4})?(\d{1})?/, "$1-$2-$3-$4-$5");
+		};	
+	})
+	.filter('barCode', function(){
+		return function(input){
+			var input = input || '';
+			return input.replace(/^(\d{1})(\d{6})?(\d{6})?/, "$1 $2 $3");
+		};	
+	})
 ;	
