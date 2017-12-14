86be461cd0db92825fb581745e65a7925dea7818
Davisson <dvisson.medeiros@gmail.com>
2017-12-14 15:11:26 -0200

    add botão para realizar troca do pedido

--- public/js/controllers/order-detail-controller.js
+++ public/js/controllers/order-detail-controller.js
@@ -1,4 +1,4 @@
-angular.module('ecommerce').controller('OrderDetailController', function($scope, $routeParams, orderResource){
+angular.module('ecommerce').controller('OrderDetailController', function($scope, $routeParams, $location, orderResource){
 
     $scope.order = {};
 
@@ -9,5 +9,21 @@ angular.module('ecommerce').controller('OrderDetailController', function($scope,
             console.log(erro);
         });
     }
+
+    $scope.changeStatusOrder = function(order){
+
+    	$scope.order.statusOrder.code = 'EXCHANGE';
+		orderResource.changeStatus(order, function(order) {
+			$scope.order = order;
+			$scope.order.items = order.items;
+			$scope.responseMessage = {
+				message : 'Emitido solicitação de troca para o pedido: ' + order.code,
+				hasError: false					
+			}			
+		}, function(error) {					
+			$scope.responseMessage = error.data;
+			console.log(error);
+		});	
+	};
 
 });
