e6b191122c3bdebdda6cb9fb4721a0542e60f891
Davisson <dvisson.medeiros@gmail.com>
2017-12-14 15:11:02 -0200

    add notificação

--- public/js/controllers/orders-controller.js
+++ public/js/controllers/orders-controller.js
@@ -1,13 +1,24 @@
-angular.module('ecommerce').controller('MyRequestsController', function($scope, orderResource){
+angular.module('ecommerce').controller('MyRequestsController', function($scope, $rootScope, orderResource, notificationResource){
 
 	$scope.message = '';
 	$scope.orders = [];
 
 	var req = {
-		"entity" : {}
+		"entity" : {},
+		logged : true
 	}
 	orderResource.filter(req, function(orders){
 		$scope.orders = orders;
+	}, function(error){
+		console.log(error);
+	});
+
+	let filter = {
+		entity : {},
+		logged : true
+	}
+	notificationResource.filter(filter, function(notifications){
+		$rootScope.notifications = notifications;
 	}, function(error){
 		console.log(error);
 	});
