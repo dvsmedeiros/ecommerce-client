bb9a65f3e9fc1619d4d5bf877806393a337845c5
Davisson <dvisson.medeiros@gmail.com>
2017-12-14 15:12:14 -0200

    add funcoes isAuthenticated e hasRole

--- public/js/controllers/login-controller.js
+++ public/js/controllers/login-controller.js
@@ -1,18 +1,37 @@
-angular.module('ecommerce').controller('LoginController', function($rootScope, $scope, $http, $location, $window, loginResource){
+angular.module('ecommerce').controller('LoginController', function($rootScope, $interval, $scope, $http, $location, $window, loginResource, cartResource, notificationResource){
 
 	$rootScope.user = {};
+	$rootScope.cartSize = 0;
 	$scope.message = '';
-
+	$rootScope.notifications = [];
+	
+	/*
+	$scope.interval = 5000;
+	$interval(function(){
+		$scope.fetchNotification();	
+	}, $scope.interval);	
+	
+	$scope.fetchNotification = function(){	
+		let filter = {
+			entity : {},
+			logged : true
+		}
+		notificationResource.filter(filter, function(notifications){
+			$rootScope.notifications = notifications;			
+		}, function(error){
+			console.log(error);
+		});
+	}
+	*/
 	$scope.authenticate = function(){	
 		
 		var req = {
 			email : $rootScope.user.login,
 			password : $rootScope.user.password
 		}
-
 		$http.post('http://localhost:8888/login', req)
-		.then(function(data){
-			$window.sessionStorage.setItem('principal', JSON.stringify($rootScope.user.login));
+		.then(function(response){
+			$window.sessionStorage.setItem('principal', JSON.stringify(response.data));
 			$location.path('/');			
 		}, function(error){
 			console.log(error);
@@ -24,7 +43,12 @@ angular.module('ecommerce').controller('LoginController', function($rootScope, $
 	
 		delete $window.sessionStorage.token;
 		delete $window.sessionStorage.principal;
-		$location.path('login');
+		cartResource.delete(function(cart){
+			$scope.cart = cart;
+			$location.path('login');
+		}, function(erro){
+			console.log(erro);
+		});
 
 	};
 
@@ -35,6 +59,14 @@ angular.module('ecommerce').controller('LoginController', function($rootScope, $
 			return false;
 		}
 	}
+
+	$rootScope.hasRole = function(role){		
+		if($window.sessionStorage.principal){
+			return angular.fromJson($window.sessionStorage.principal).authorities.find(e => e.authority === role) ? true : false;
+		} else {
+			return false;
+		}
+	}	
 
 
 
